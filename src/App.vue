aa5b951f132c86f828ac4d3142a2406c48a0d916
cedricrenouleau <ced241@gmail.com>
2024-07-21 22:41:43 +0200

    clean code

--- src/App.vue
+++ src/App.vue
@@ -32,13 +32,4 @@ const logout = () => {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
